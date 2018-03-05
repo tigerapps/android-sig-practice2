d3755fb6904d7ce9b396149e5c794a9d683e056a
Samuel Holland <samuel@sholland.org>
2018-03-05 13:03:33 -0600

    Add a function to get a stylized repo path

--- app/src/main/java/edu/missouri/mca/android/practice2/model/Repo.java
+++ app/src/main/java/edu/missouri/mca/android/practice2/model/Repo.java
@@ -1,5 +1,10 @@
 package edu.missouri.mca.android.practice2.model;
 
+import android.graphics.Typeface;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
+
 /**
  * Created by samuel on 2/19/18.
  */
@@ -24,5 +29,13 @@ public class Repo {
 
     public GitHubUser getOwner() {
         return owner;
+    }
+
+    public CharSequence getPath() {
+        final SpannableStringBuilder ssb = new SpannableStringBuilder();
+        ssb.append(owner.getLogin());
+        ssb.append(" / ", new StyleSpan(Typeface.BOLD), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.append(name);
+        return ssb;
     }
 }
